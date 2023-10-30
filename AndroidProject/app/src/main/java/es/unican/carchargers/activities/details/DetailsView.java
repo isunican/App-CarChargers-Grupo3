55e9d6760ca36a5986fd2e5fe12e708b2bc4ab52
ynb568 <100769095+ynb568@users.noreply.github.com>
2023-10-30 12:01:42 +0100

    Cuenta comentarios creo que bien

--- AndroidProject/app/src/main/java/es/unican/carchargers/activities/details/DetailsView.java
+++ AndroidProject/app/src/main/java/es/unican/carchargers/activities/details/DetailsView.java
@@ -54,7 +54,7 @@ public class DetailsView extends AppCompatActivity  {
         TextView tvWeb = findViewById(R.id.tvPaginaWeb);
         //Web que muestra el mapa
         WebView webview = findViewById(R.id.web);
-        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setJavaScriptEnabled(false);
         //Lista de comentarios
         TextView tvComment = findViewById(R.id.tvCommentsCount);
         ListView lvComments = findViewById(R.id.lvComments);
@@ -146,11 +146,10 @@ public class DetailsView extends AppCompatActivity  {
         tvWeb.setText(charger.operator.website);
 
         //Cálculo de numero de comentarios
-
         if (charger.getChargerComments() == 0) {
             tvComment.setText("Comentarios (0)");
         } else {
-            tvComment.setText("Comentarios (" + charger.getChargerComments() + ")");
+            tvComment.setText("Comentarios (" + String.valueOf(charger.getChargerComments()) + ")");
         }
 
         //Muestreo de comentarios
